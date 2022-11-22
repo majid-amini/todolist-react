9db9d498fb0614590a5f7a943d2c87087d828e5e
majidamini <majidamini_s@yahoo.com>
2022-11-22 19:18:57 +0330

    add a new state for validation and new styles for changing input's background color when it is empty

--- src/components/FormItems.js
+++ src/components/FormItems.js
@@ -2,13 +2,17 @@ import React, { useState } from "react";
 import "./FormItems.css";
 const FormItems = (props) => {
   const [todo, setTodo] = useState("");
-
+  const [isValid, setIsValid] = useState(true);
   const changeHandler = (e) => {
     setTodo(e.target.value);
+    if (e.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
   };
   const submitHandler = (e) => {
     e.preventDefault();
     if (todo.trim().length === 0) {
+      setIsValid(false);
       return;
     }
     props.addItemHandler(todo);
@@ -19,6 +23,7 @@ const FormItems = (props) => {
       <form onSubmit={submitHandler} className="form">
         <h3>Todo List</h3>
         <input
+          style={{ backgroundColor: !isValid ? "#fab1a0" : "" }}
           onChange={changeHandler}
           placeholder="writing your tasks here"
           type="text"
